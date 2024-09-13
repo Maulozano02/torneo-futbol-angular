8e446fa931952015d69089eb17b12adc8b66762d
David Julio Faudoa Gonzalez <a00834801@tec.mx>
2024-09-12 23:56:04 -0600

    Update app-routing.ts
    .

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { TorneosComponent } from './torneos/torneos.component';
 
 export const routes: Routes = [
-  //{ path: 'home/:id/:name', component: HomeComponent }, // Ruta a HomeComponent
+  { path: 'home/:id/:name', component: HomeComponent }, // Ruta a HomeComponent
   { path: 'torneos', component: TorneosComponent }, // Ruta a TorneosComponent
   { path: '', redirectTo: '/torneos', pathMatch: 'full' } // Redirección a Home
 ];
